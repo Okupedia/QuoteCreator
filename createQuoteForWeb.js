72cb980590a6eba155833242890f5de22348417f
Okupedia <Okupedia@icloud.com>
2022-08-30 20:27:51 +0900

    dataProviderButtonの実装

--- createQuoteForWeb.js
+++ createQuoteForWeb.js
@@ -1,71 +1,72 @@
-var text = "";
-var quoteTextArea = document.getElementById('quoteTextArea');
+const quoteTextArea = document.getElementById('quoteTextArea');
 
-var today = new Date();
+const today = new Date();
 today.setDate(today.getDate());
 document.getElementById("accessedYear").value = today.getFullYear();
 document.getElementById("accessedMonth").value = ("0"+(today.getMonth()+1)).slice(-2);
 document.getElementById("accessedDay").value = ("0"+today.getDate()).slice(-2);
 
-var dataProviderButton = document.getElementById('dataProviderButton');
-dataProviderButton.onclick = function () {
-    document.getElementById("dataProvider").value = document.getElementById('author').value;
-}
+window.onload = function () {
+    const dataProviderButton = document.getElementById('dataProviderButton');
+    dataProviderButton.onclick = function () {
+        document.getElementById("dataProvider").value = document.getElementById('author').value;
+    }
 
-var japaneseIPSJWebButton = document.getElementById('japaneseIPSJWebButton');
-japaneseIPSJWebButton.onclick = function () {
-    console.log("japanese button clickecd");
-    text=createQuoteOfWebForIPSJ("，", "：", "．", "ほか", "（", "）", "（オンライン）", "入手先", "参照")
-    quoteTextArea.innerHTML = text;
-}
+    const japaneseIPSJWebButton = document.getElementById('japaneseIPSJWebButton');
+    japaneseIPSJWebButton.onclick = function () {
+        console.log("japanese button clickecd");
+        const text=createQuoteOfWebForIPSJ("，", "：", "．", "ほか", "（", "）", "（オンライン）", "入手先", "参照")
+        quoteTextArea.innerHTML = text;
+    }
 
-var englishIPSJWebButton = document.getElementById('englishIPSJWebButton');
-englishIPSJWebButton.onclick = function () {
-    console.log("english button clickecd");
-    text=createQuoteOfWebForIPSJ(", ", ": ", ".", ", et al.", "(", ")", "(online)", "available from", "accessed ")
-    quoteTextArea.innerHTML = text;
+    const englishIPSJWebButton = document.getElementById('englishIPSJWebButton');
+    englishIPSJWebButton.onclick = function () {
+        console.log("english button clickecd");
+        const text=createQuoteOfWebForIPSJ(", ", ": ", ".", ", et al.", "(", ")", "(online)", "available from", "accessed ")
+        quoteTextArea.innerHTML = text;
+    }
 }
 
 function createQuoteOfWebForIPSJ(comma, colon, period, etAlii, openBrackets, closeBrackets, online, available, accessed){
     var text = "";
-    var errorText = "著者(Webサイトの名称)・Wepページの題名・入手先・URLは必須です";
+    const errorText = "著者(Webサイトの名称)・Wepページの題名・入手先・URLは必須です";
     //著者情報
-    var author = document.getElementById('author').value;
+    const author = document.getElementById('author').value;
     if (!author) {
         return errorText;
     }
     text = text + author;
     //Webページの題名
-    var title = document.getElementById('webTitle').value;
+    const title = document.getElementById('webTitle').value;
     if (!title) {
         return errorText;
     }
     text = text + colon + title + comma;
     //Webページの名称
-    var webName = document.getElementById('webName').value;
+    const webName = document.getElementById('webName').value;
     if (webName) {
         text = text + webName + comma;
     }
     //入手先
-    var dataProvider = document.getElementById('dataProvider').value;
+    const dataProvider = document.getElementById('dataProvider').value;
     if (!title) {
         return errorText;
     }
     text = text  + dataProvider + online;
     //URL
-    var webUrl = document.getElementById('webUrl').value;
+    const webUrl = document.getElementById('webUrl').value;
     if (!webUrl) {
         return errorText;
     }
     text = text + comma+ available + "〈"  + webUrl + "〉" ;
     //発行年
-    var accessedYear = document.getElementById('accessedYear').value;
+    const accessedYear = document.getElementById('accessedYear').value;
     if (accessedYear) {
         text = text + openBrackets + accessed + accessedYear;
-        var accessedMonth = document.getElementById('accessedMonth').value;
+        const accessedMonth = document.getElementById('accessedMonth').value;
         if (accessedMonth) {
             text = text + "-" + accessedMonth;
-            var accessedDay = document.getElementById('accessedDay').value;
+            const accessedDay = document.getElementById('accessedDay').value;
             if (accessedDay) {
                 text = text + "-" + accessedDay;
             }
